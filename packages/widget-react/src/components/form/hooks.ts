ff4e8ff6c8153f279328e5058950b7a66bf5bf3e
sim <sim@cheolhwan.com>
2025-05-23 12:48:23 +0900

    feat: prevent autofocus on mobile device

--- packages/widget-react/src/components/form/hooks.ts
+++ packages/widget-react/src/components/form/hooks.ts
@@ -1,11 +1,14 @@
 import { useEffect, useRef } from "react"
+import { useMedia } from "react-use"
 
 export function useAutoFocus<T extends HTMLInputElement>() {
   const ref = useRef<T>(null)
+  const isSmall = useMedia("(max-width: 576px)")
 
   useEffect(() => {
+    if (isSmall) return
     ref.current?.focus()
-  }, [])
+  }, [isSmall])
 
   return ref
 }
