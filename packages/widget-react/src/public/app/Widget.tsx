f5693e808675ce575f1f26a7b9bd644b396242d8
sim <sim@cheolhwan.com>
2025-06-13 21:11:01 +0900

    feat(bridge): allow initial bridge form values as props for embedded usage

--- packages/widget-react/src/public/app/Widget.tsx
+++ packages/widget-react/src/public/app/Widget.tsx
@@ -1,11 +1,12 @@
 import { MemoryRouter } from "@/lib/router"
 import AsyncBoundary from "@/components/AsyncBoundary"
+import type { FormValues } from "@/pages/bridge/data/form"
 import { fullscreenContext } from "./fullscreen"
 import Routes from "./Routes"
 
-const Widget = ({ bridge }: { bridge?: boolean }) => {
+const Widget = ({ bridge }: { bridge?: Partial<FormValues> }) => {
   return (
-    <MemoryRouter initialEntry={{ path: bridge ? "/bridge" : "/" }}>
+    <MemoryRouter initialEntry={{ path: bridge ? "/bridge" : "/", state: bridge }}>
       <div className="body">
         <fullscreenContext.Provider value={!!bridge}>
           <AsyncBoundary>
