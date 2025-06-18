b9aeee2568b19e5dafb801e70b3237acaa10fdc9
alecande11 <54709706+alecande11@users.noreply.github.com>
2025-06-18 11:01:02 +0200

    Fix cropped tooltips

--- packages/widget-react/src/components/WidgetTooltip.tsx
+++ packages/widget-react/src/components/WidgetTooltip.tsx
@@ -2,18 +2,29 @@ import type { PropsWithChildren } from "react"
 import { Tooltip } from "radix-ui"
 import { usePortal } from "@/public/app/PortalContext"
 import styles from "./WidgetTooltip.module.css"
+import clsx from "clsx"
 
 interface Props {
   label: string
   disableHoverableContent?: boolean
+  small?: boolean
 }
 
-const WidgetTooltip = ({ label, children, disableHoverableContent }: PropsWithChildren<Props>) => {
+const WidgetTooltip = ({
+  label,
+  children,
+  disableHoverableContent,
+  small,
+}: PropsWithChildren<Props>) => {
   return (
     <Tooltip.Root disableHoverableContent={disableHoverableContent}>
       <Tooltip.Trigger asChild>{children}</Tooltip.Trigger>
       <Tooltip.Portal container={usePortal()}>
-        <Tooltip.Content className={styles.tooltip}>
+        <Tooltip.Content
+          className={clsx(styles.tooltip, { small })}
+          collisionBoundary={usePortal()}
+          collisionPadding={8}
+        >
           {label}
           <Tooltip.Arrow className={styles.arrow} />
         </Tooltip.Content>
