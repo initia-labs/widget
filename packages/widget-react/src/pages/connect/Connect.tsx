e932151888bf839a049c79ade5efb8a3d6250e88
sim <sim@cheolhwan.com>
2025-07-05 16:30:37 +0900

    feat(connect): display recommended wallets when no wallet is installed

--- packages/widget-react/src/pages/connect/Connect.tsx
+++ packages/widget-react/src/pages/connect/Connect.tsx
@@ -2,10 +2,19 @@ import type { Connector } from "wagmi"
 import { useConnect } from "wagmi"
 import { useState } from "react"
 import { useMutation } from "@tanstack/react-query"
+import { IconExternalLink, IconWarningFilled } from "@initia/icons-react"
 import { normalizeError } from "@/data/http"
 import { useWidgetVisibility } from "@/data/ui"
 import List from "@/components/List"
+import Image from "@/components/Image"
 import styles from "./Connect.module.css"
+
+const recommendedWallets = [
+  { name: "Rabby", url: "https://rabby.io" },
+  { name: "Phantom", url: "https://phantom.com" },
+  { name: "Keplr", url: "https://keplr.app" },
+  { name: "Leap", url: "https://leapwallet.io" },
+]
 
 const Connect = () => {
   const { closeWidget } = useWidgetVisibility()
@@ -27,6 +36,32 @@ const Connect = () => {
       closeWidget()
     },
   })
+
+  if (connectors.length === 0) {
+    return (
+      <div className={styles.empty}>
+        <IconWarningFilled size={36} className={styles.icon} />
+        <h1>No wallets detected</h1>
+        <p>
+          Compatible with most EVM wallets.
+          <br />
+          Here are some popular options we recommend.
+        </p>
+        <div className={styles.list}>
+          {recommendedWallets.map(({ name, url }) => {
+            const imageUrl = `https://assets.initia.xyz/images/wallets/${name}.webp`
+            return (
+              <a href={url} className={styles.item} target="_blank" key={name}>
+                <Image src={imageUrl} width={24} height={24} />
+                <span className={styles.name}>{name}</span>
+                <IconExternalLink size={10} />
+              </a>
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
