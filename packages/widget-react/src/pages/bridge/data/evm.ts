b62caba413874c1c55ede60abdcd90d293ca4a4d
alecande11 <54709706+alecande11@users.noreply.github.com>
2025-07-09 11:48:05 +0200

    Improve chain validation before switching

--- packages/widget-react/src/pages/bridge/data/evm.ts
+++ packages/widget-react/src/pages/bridge/data/evm.ts
@@ -6,7 +6,11 @@ interface ProviderError extends Error {
 }
 
 export async function switchEvmChain(provider: BrowserProvider, chain: RouterChainJson) {
-  const { chain_id, chain_name, evm_fee_asset, rpc } = chain
+  const { chain_id, chain_name, evm_fee_asset, rpc, chain_type } = chain
+
+  if (chain_type !== "evm")
+    throw new Error(`Unable to switch to ${chain_name} [${chain_id}], it is not an EVM chain`)
+
   try {
     await provider.send("wallet_switchEthereumChain", [
       { chainId: `0x${Number(chain_id).toString(16)}` },
@@ -15,6 +19,9 @@ export async function switchEvmChain(provider: BrowserProvider, chain: RouterCha
     const e = _e as ProviderError
     // ensure wallet_switchEthereumChain failed due to chain not being added
     if (e.code !== 4902) throw e
+
+    if (!evm_fee_asset)
+      throw new Error(`Unable to add ${chain_name} [${chain_id}], no 'evm_fee_asset' provided`)
 
     // request wallet to add the chain
     await provider.send("wallet_addEthereumChain", [
