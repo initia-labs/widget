99a630cbe35d8cbb15abb56ee0174bcc39fe75ba
alecande11 <54709706+alecande11@users.noreply.github.com>
2025-07-09 11:44:54 +0200

    ensure wallet_switchEthereumChain failed due to the chain not being added

--- packages/widget-react/src/pages/bridge/data/evm.ts
+++ packages/widget-react/src/pages/bridge/data/evm.ts
@@ -1,14 +1,22 @@
 import type { BrowserProvider } from "ethers"
 import type { RouterChainJson } from "./chains"
 
+interface ProviderError extends Error {
+  code?: number
+}
+
 export async function switchEvmChain(provider: BrowserProvider, chain: RouterChainJson) {
   const { chain_id, chain_name, evm_fee_asset, rpc } = chain
   try {
     await provider.send("wallet_switchEthereumChain", [
       { chainId: `0x${Number(chain_id).toString(16)}` },
     ])
-  } catch {
-    // Chain not added, request user to add it
+  } catch (_e) {
+    const e = _e as ProviderError
+    // ensure wallet_switchEthereumChain failed due to chain not being added
+    if (e.code !== 4902) throw e
+
+    // request wallet to add the chain
     await provider.send("wallet_addEthereumChain", [
       {
         chainId: `0x${Number(chain_id).toString(16)}`,
